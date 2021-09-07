55c5c2703055714e30ff52053ef8ee18e43664c0
Binesh Daruru <72850126+DaruruBinesh@users.noreply.github.com>
2021-09-07 09:45:17 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Q2{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+if(n%3 == 0 && n%5 == 0){
+System.out.println("It is divisible by 3 and 5");
+}
+else{
+System.out.println("It is not divisible by 3 and 5");
+}
+System.out.println(" ");
+System.out.println("By ");
+System.out.println(" Student Name : Daruru Binesh ");
+System.out.println(" Reg.No : 18BCE0872 ");
+}
+}
